bed44b9a1d90459b42c7472ce75ff057ff4c717c
BICHENG <1819775678@qq.com>
2024-06-03 15:31:16 +0800

    1s

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
-from setuptools import setup, find_packages
-from setuptools.command.install import install as _install
+from setuptools import setup, find_packages, Command
 from datetime import datetime
 
 # 检查是否安装了 OpenCV
@@ -35,10 +34,18 @@ if os.getenv('RELEASE_VERSION'):
 else:
     full_version = f"{base_version_next}.dev{date_suffix}"
 
-class PostInstallCommand(_install):
+class PostInstallCommand(Command):
     """Post-installation for installation mode."""
+    description = "Run post-installation tasks"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
     def run(self):
-        _install.run(self)
         print("\n" + "="*50)
         print("🎉 \033[1mInstallation complete! Thank you for installing func2stream.\033[0m 🎉")
         print("🔄 Effortlessly transform functions into asynchronous elements for building high-performance pipelines.\n")
